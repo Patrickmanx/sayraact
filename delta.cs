59e5d1c27b02e53c83c018ab7dcb2588f5414b56
Patrickmanx <60354462+Patrickmanx@users.noreply.github.com>
2023-11-17 13:50:04 -0300

    Update delta.cs

--- delta.cs
+++ delta.cs
@@ -21,7 +21,7 @@ namespace SplatoonScriptsOfficial.Duties.Endwalker.The_Omega_Protocol
     {
         public override HashSet<uint> ValidTerritories => new() { 1122 };
 
-        public override Metadata? Metadata => new(7, "NightmareXIV");
+        public override Metadata? Metadata => new(7, "Maelyn");
 
         Config Conf => Controller.GetConfig<Config>();
 
